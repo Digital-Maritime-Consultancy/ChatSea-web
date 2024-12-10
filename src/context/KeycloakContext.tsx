b05635c91e792f4d8711d2d600f70dc10189fbef
Jinki <your.jinki.jung@gmail.com>
2024-12-10 12:51:44 +0100

    keycloak session handling

--- src/context/KeycloakContext.tsx
+++ src/context/KeycloakContext.tsx
@@ -23,12 +23,13 @@ import React, {
   }
 
     const KeycloakProvider: React.FC<KeycloakProviderProps> = ({ children }) => {
-    const navigate = useNavigate()
-    const isRun = useRef<boolean>(false)
-    const [keycloak, setKeycloak] = useState<Keycloak | null>(null)
-    const [authenticated, setAuthenticated] = useState<boolean>(false)
-    const [mrn, setMrn] = useState<string>("")
-    const [username, setUsername] = useState<string>("")
+    const navigate = useNavigate();
+    const isRun = useRef<boolean>(false);
+    const [keycloak, setKeycloak] = useState<Keycloak | null>(null);
+    const [authenticated, setAuthenticated] = useState<boolean>(false);
+    const [mrn, setMrn] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+
   
     useEffect(() => {
       if (isRun.current) return; 
@@ -44,6 +45,18 @@ import React, {
         }
         const keycloakInstance: Keycloak = new Keycloak(keycloackConfig)
   
+        keycloakInstance.onTokenExpired = () => {
+          console.log('expired '+new Date());
+          keycloakInstance.updateToken(50).success((refreshed)=>{
+                if (refreshed){
+                    console.log('refreshed '+new Date());
+                }else {
+                    console.log('not refreshed '+new Date());
+                }
+            }).error(() => {
+                 console.error('Failed to refresh token '+new Date());
+            });
+        }
         keycloakInstance
           .init({
             onLoad: 'check-sso',
@@ -55,7 +68,7 @@ import React, {
             if (!authenticated) {
               navigate('/');
             } else {
-                navigate('/connect');
+              navigate('/connect');
             }
           })
           .catch((error) => {
